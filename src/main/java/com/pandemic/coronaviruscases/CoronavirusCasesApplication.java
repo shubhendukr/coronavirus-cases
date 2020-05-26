e0d6b73fd1fca6bb4cac7d40d92c61687a01ac95
Shubhendu Kumar <kumar.shubhendu.sg@gmail.com>
2020-05-27 01:32:00 +0800

    refs #000000 initial commit for initialzing project

---
+++ src/main/java/com/pandemic/coronaviruscases/CoronavirusCasesApplication.java
@@ -0,0 +1,13 @@
+package com.pandemic.coronaviruscases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusCasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandemic/coronaviruscases/CoronavirusCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandemic.coronaviruscases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
