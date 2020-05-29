26a05233a90a3f41869bf566d536cbc6f3bdb0c3
Shubhendu Kumar <kumar.shubhendu.sg@gmail.com>
2020-05-29 20:31:56 +0800

    refs #000003 adding csv parser for fetched coronavirus record

--- src/main/java/com/pandemic/coronaviruscases/service/CoronaVirusDataService.java
+++ src/main/java/com/pandemic/coronaviruscases/service/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package com.pandemic.coronaviruscases.service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,6 +25,11 @@ public class CoronaVirusDataService {
                 .build();
 
         HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+        }
     }
 }
 
